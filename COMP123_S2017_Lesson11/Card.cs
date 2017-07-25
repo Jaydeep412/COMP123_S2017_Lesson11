ab54b09500ba42dd28054d5c9ec007b520970f61
Jaydeep <jlakhawani@gmail.com>
2017-07-25 15:28:03 -0400

    Created the Card class.

---
+++ COMP123_S2017_Lesson11/Card.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2017_Lesson11
+{
+    public class Card
+    {
+        //PRIVATE INSTANCE VARIABLES (FIELDS)
+        private Face _face;
+        private Suit _suit;
+
+        //PUBLIC PROPERTIES
+        public Face Face
+        {
+            get
+            {
+                return this._face;
+            }
+            set
+            {
+                this._face = value;
+            }
+        }
+
+        public Suit Suit
+        {
+            get
+            {
+                return this._suit;
+            }
+            set
+            {
+                this._suit = value;
+            }
+        }
+
+        //CONSTRUCTOR
+        /// <summary>
+        /// This is the main constructor for Card class;
+        /// It takes two parameters - face(Face) - suit(Suit)
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="suit"></param>
+        public Card(Face face, Suit suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
