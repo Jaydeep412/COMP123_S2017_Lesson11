c470f4beffb1b59e8dcf60377132a5c79a89bb9e
Jaydeep <jlakhawani@gmail.com>
2017-07-25 15:56:54 -0400

    updated the program class to implement/test the deck class

--- COMP123_S2017_Lesson11/Program.cs
+++ COMP123_S2017_Lesson11/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name : Jaydeep Lakhwani
  * Date : 25th July, 2017
  * Description : Lesson-11
- * Version 0.1 - Created the program class.
+ * Version 0.2 - Implemented the deck class.
  */
 
 namespace COMP123_S2017_Lesson11
@@ -17,6 +17,8 @@ namespace COMP123_S2017_Lesson11
     {
         static void Main(string[] args)
         {
+            Deck deck = new Deck();
+            Console.WriteLine(deck.ToString());
         }
     }
 }
