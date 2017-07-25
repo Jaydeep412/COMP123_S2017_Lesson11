e2a11447260dd7a3bdc644c351d27492f23e385b
Jaydeep <jlakhawani@gmail.com>
2017-07-25 16:29:00 -0400

    started shuffle method

--- COMP123_S2017_Lesson11/Deck.cs
+++ COMP123_S2017_Lesson11/Deck.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Name : Jaydeep Lakhwani
  * Date : 25th July, 2017
  * Description : This is the Deck class which inhertis from Card list
- * Version 0.3 - Fixed bug in ToString method
+ * Version 0.4 - Added shuffle method
  */
 
 namespace COMP123_S2017_Lesson11
@@ -15,7 +15,20 @@ namespace COMP123_S2017_Lesson11
     public class Deck: List<Card>
     {
         //PRIVATE INSTANCE VARIABLES (FIELDS)
+        private Random _random;
 
+        //Private PROPERTIES
+        //private int Random
+        //{
+        //    get
+        //    {
+
+        //    }
+        //    set
+        //    {
+
+        //    }
+        //}
         //PUBLIC PROPERTIES
 
         //CONSTRUCTOR
@@ -24,9 +37,17 @@ namespace COMP123_S2017_Lesson11
             this._initialize();
         }
         //PRIVATE METHODS
+        /// <summary>
+        /// This is the private _initialize method that loads the deck with 52 cards.
+        /// This method also initializes other class variables.
+        /// </summary>
 
         private void _initialize()
         {
+            //initialize the random object
+          //  this._random = 
+
+            //load the class with cards
             for (int suit=0; suit<= (int)Suit.Spades; suit++)
             {
                 for (int face = 1; face <= (int)Face.King; face++)
@@ -50,5 +71,17 @@ namespace COMP123_S2017_Lesson11
             }
             return outString;
         }
+
+        //public void Shuffle()
+        //{
+        //    int firstCard;
+        //    int secondCard;
+
+        //    firstCard = this.Random.Next(0, 52);
+        //    secondCard = this.Random.Next(0, 52);
+
+
+        //}
+
     }
 }
